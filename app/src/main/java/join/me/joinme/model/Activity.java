8b6f89da08be8342236f5784e4939e40303339f7
Mahammad Ismayilzada <gmahammad12@gmail.com>
2015-09-19 04:33:51 -0400

    added model classesand facebook sdk

---
+++ app/src/main/java/join/me/joinme/model/Activity.java
@@ -0,0 +1,14 @@
+package join.me.joinme.model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by mismayil on 19/09/15.
+ */
+public class Activity {
+    private User owner;
+    private String action;
+    private Location location;
+    private ArrayList<User> buddies;
+    private ArrayList<Cheer> cheers;
+}
---
+++ app/src/main/java/join/me/joinme/model/Location.java
@@ -0,0 +1,7 @@
+package join.me.joinme.model;
+
+/**
+ * Created by mismayil on 19/09/15.
+ */
+public class Location {
+}
